44bfbe54f04fd064fe9e67c72af0fe5e155b2710
Son Bui <sonbv@smartosc.com>
2016-05-31 13:03:18 +0700

    finish ch03

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from lists.views import home_page
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', home_page, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
