b0cf685d02aa137afcd7b3994e607d06e4b2e32d
Son Bui <sonbv@smartosc.com>
2016-06-05 12:49:53 +0700

    Chapter 04

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -12,6 +14,28 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to=do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feather")
+        # Hit enter
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feather' for row in rows),
+            "New to-do item did not appear in table"
+        )
+
         self.fail('Finish the test!')
 
 
